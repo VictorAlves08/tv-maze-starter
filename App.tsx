3b3be4158a5fc8229fea373f8120228d82cb5c7a
VictorAlves08 <victoralvees17@gmail.com>
2023-11-18 23:56:24 -0300

    feat: Create Container component

--- App.tsx
+++ App.tsx
@@ -1,33 +1,22 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View } from "react-native";
 import { ThemeProvider } from "styled-components/native";
 import { defaultTheme } from "./src/common/constants/styles/theme/defaultTheme";
 
 import Text from "./src/common/components/Text/index";
 import Spacer from "./src/common/components/Spacer";
+import Container from "./src/common/components/Container";
 
 export default function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <View style={styles.container}>
-        <Text size={24} color="brand">
-          Running Text Component!
+      <Container>
+        <Text size={30} color="alert">
+          Oi Leticia to criando um app!
         </Text>
         <Spacer height={20} />
-        <Text size={24} color="primary">
+        <Text size={30} color="onSecondary">
           Running Text Component!
         </Text>
-        <StatusBar style="auto" />
-      </View>
+      </Container>
     </ThemeProvider>
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+};
