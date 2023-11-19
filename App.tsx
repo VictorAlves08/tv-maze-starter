bfd0f8f9d07d3ae016340beb1726bee56746f8fa
VictorAlves08 <victoralvees17@gmail.com>
2023-11-19 00:43:01 -0300

    feat: Create safeAreaView Component

--- App.tsx
+++ App.tsx
@@ -4,18 +4,21 @@ import { defaultTheme } from "./src/common/constants/styles/theme/defaultTheme";
 import Text from "./src/common/components/Text/index";
 import Spacer from "./src/common/components/Spacer";
 import Container from "./src/common/components/Container";
+import Content from "./src/common/components/Content";
 
 export default function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <Container>
-        <Text size={30} color="alert">
-          Running Text Component!
-        </Text>
-        <Spacer height={20} />
-        <Text size={30} color="onSecondary">
-          Running Text Component!
-        </Text>
+        <Content>
+          <Text size={30} color="alert">
+            Running Text Component 1!
+          </Text>
+          <Spacer height={20} />
+          <Text size={30} color="onSecondary">
+            Running Text Component 2!
+          </Text>
+        </Content>
       </Container>
     </ThemeProvider>
   );
