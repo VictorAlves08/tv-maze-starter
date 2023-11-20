ee502479a574d375ff5909a6213945529b49a3d2
VictorAlves08 <victoralvees17@gmail.com>
2023-11-19 22:32:38 -0300

    feat: Create notFound Component

--- App.tsx
+++ App.tsx
@@ -6,13 +6,20 @@ import Spacer from "./src/common/components/Spacer";
 import Container from "./src/common/components/Container";
 import Content from "./src/common/components/Content";
 import Icon from "./src/common/components/Icon";
+import BackButton from "./src/common/components/BackButton";
 import Avatar from "./src/common/components/Avatar";
+import NotFound from "./src/common/components/NotFound";
 
 export default function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <Container>
         <Content>
+          <BackButton
+            onPress={() => {
+              console.log("Clicked!");
+            }}
+          />
           <Text size={30} color="alert">
             Running Text Component 1!
           </Text>
@@ -20,6 +27,8 @@ export default function App() {
           <Icon icon="home" size={40} color="#fff" />
           <Spacer height={20} />
           <Avatar size="large" url="https://i.pravatar.cc/300" />
+          <Spacer height={20} />
+          <NotFound />
         </Content>
       </Container>
     </ThemeProvider>
