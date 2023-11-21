fd24dcf78decf73ac9c180effecfe702add3ccd8
VictorAlves08 <victoralvees17@gmail.com>
2023-11-20 23:07:38 -0300

    feat: Create navigation and request components

--- App.tsx
+++ App.tsx
@@ -1,33 +1,16 @@
+import { NavigationContainer } from "@react-navigation/native";
 import { ThemeProvider } from "styled-components/native";
+
 import { defaultTheme } from "./src/common/constants/styles/theme/defaultTheme";
 
-import { episodeMocked } from "./src/common/constants/mocks/episode.mock";
-
-import Container from "./src/common/components/Container";
-import Content from "./src/common/components/Content";
-import Text from "./src/common/components/Text/index";
-import Spacer from "./src/common/components/Spacer";
-import Icon from "./src/common/components/Icon";
-import BackButton from "./src/common/components/BackButton";
-import Avatar from "./src/common/components/Avatar";
-import NotFound from "./src/common/components/NotFound";
-import Input from "./src/common/components/Input";
-import ShowCover from "./src/common/components/ShowCover";
-import EpisodeCover from "./src/common/components/EpisodeCover";
-import EpisodeCard from "./src/common/components/EpisodeCard";
+import BottomTabNavigator from "./src/navigation/BottomTabNavigator/index";
 
 export default function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Container>
-        <Content>
-          <EpisodeCover
-            size="large"
-            url="https://static.tvmaze.com/uploads/images/medium_landscape/15/38639.jpg"
-          />
-          <EpisodeCard episode={episodeMocked} />
-        </Content>
-      </Container>
+      <NavigationContainer>
+        <BottomTabNavigator />
+      </NavigationContainer>
     </ThemeProvider>
   );
 };
---
+++ src/screens/Home/home.view.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { View } from "react-native";
+import useHomeController from "./home.controller";
+
+// import {Container} from './styles'
+
+const Home: React.FC = () => {
+    const { loading, shows } = useHomeController();
+
+    return <View style={{ flex: 1, backgroundColor: "pink" }} />;
+};
+
+export default Home;
