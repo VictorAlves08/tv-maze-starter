2469b630d070c96604117fb76a2580936ce73807
VictorAlves08 <victoralvees17@gmail.com>
2023-12-22 22:13:15 -0300

    Implement details with mock data

---
+++ src/screens/Detail/detail.controller.tsx
@@ -0,0 +1,94 @@
+import { useEffect, useState, useMemo } from "react";
+import { format } from "date-fns";
+import { EpisodeModel } from "../../common/models/episode.model";
+import { SeasonModel } from "../../common/models/season.model";
+import { removeHtmlFromString } from "../../common/utils/html";
+import { noSummaryString } from "../../common/utils/message";
+import { fetchEpisodesBySeason } from "../../repositories/episodes/episodes.repository";
+import { fetchSeasons } from "../../repositories/seasons/seasons.repository";
+import { UseDetailController } from "./types";
+
+const useDetailController = ({ show }: UseDetailController) => {
+    /**
+     * States
+     */
+    const [moreSummary, setMoreSummary] = useState<boolean>(false);
+    const [seasons, setSeasons] = useState<Array<SeasonModel>>([]);
+    const [selectedSeason, setSelectedSeason] = useState<SeasonModel>();
+    const [episodes, setEpisodes] = useState<EpisodeModel[]>([]);
+
+    /**
+     * Memos
+     */
+    const summaryWithoutHtml = useMemo(() => {
+        if (show.summary) {
+            return removeHtmlFromString(show.summary);
+        }
+
+        return noSummaryString;
+    }, [show]);
+
+    const formattedDate = useMemo(() => {
+        if (show.status === "Running") {
+            return format(new Date(), "PP");
+        } else if (show?.ended) {
+            return format(new Date(show.ended), "PP");
+        } else {
+            return "N/A";
+        }
+    }, [show]);
+
+    const genres = useMemo(() => {
+        return show.genres.join(", ");
+    }, [show]);
+
+    const schedule = useMemo(() => {
+        const { days, time } = show.schedule;
+        let schedule = "";
+        if (days.length > 0) {
+            schedule = days.join(", ");
+        }
+        if (time) {
+            schedule += `${schedule} at ${time}`;
+        }
+        return schedule;
+    }, [show]);
+
+    /**
+     * Callback
+     */
+    const toggleMoreSummary = () => setMoreSummary((old) => !old);
+
+    /**
+     * Effects
+     */
+    useEffect(() => {
+        fetchSeasons({ showId: show.id }).then(setSeasons);
+    }, [show]);
+
+    useEffect(() => {
+        if (seasons.length > 0) {
+            setSelectedSeason(seasons[0]);
+        }
+    }, [seasons]);
+
+    useEffect(() => {
+        if (selectedSeason) {
+            fetchEpisodesBySeason({ seasonId: selectedSeason.id }).then(setEpisodes);
+        }
+    }, [selectedSeason]);
+
+    return {
+        summaryWithoutHtml,
+        formattedDate,
+        genres,
+        schedule,
+        moreSummary,
+        toggleMoreSummary,
+        seasons,
+        selectedSeason,
+        episodes,
+    };
+};
+
+export default useDetailController;
--- src/screens/Detail/detail.view.tsx
+++ src/screens/Detail/detail.view.tsx
@@ -1,9 +1,30 @@
+import { useRoute } from "@react-navigation/native";
 import React from "react";
 import { View } from "react-native";
+import useDetailController from "./detail.controller";
 
 // import {Container} from './styles'
 
 const Detail: React.FC = () => {
+    /**
+     * Navigation
+     */
+    const {
+        params: { show },
+    } = useRoute<DetailRouteProp>();
+
+    const {
+        episodes,
+        seasons,
+        formattedDate,
+        genres,
+        moreSummary,
+        schedule,
+        selectedSeason,
+        summaryWithoutHtml,
+        toggleMoreSummary,
+    } = useDetailController({ show });
+
     return <View style={{ flex: 1, backgroundColor: "red" }} />;
 };
 
